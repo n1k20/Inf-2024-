d214877a58594ea5ce357ae1dbc2a4a5b867d4c5
n1k <nik.borgoyakov@bk.ru>
2024-12-03 20:14:00 +0700

    COMMIT add text in 19 - 21

--- Шаблон 23 зад ЕГЭ.py
+++ Шаблон 23 зад ЕГЭ.py
@@ -1,44 +1,49 @@
 from functools import *
 
+
+"""
+Прошу не пинать меня за это га*** код, но у меня просто тогда 
+не было понимания PEP 8 и того как это не эстетично
+"""
 s = set()  # множество
 
 
 @lru_cache(None)
-def f(x, y):
-    if y == 68:  # если нужное кол во команд
+def function1(x, y):
+    if y == 68:  # если нужное кол-во команд
         s.add(x)
     else:
-        f(x + 3, y + 1)
-        f(x - 2, y + 1)
+        function1(x + 3, y + 1)
+        function1(x - 2, y + 1)
 
 
-f(1, 0)
+function1(1, 0)
 print(len(s))
 
 
 @lru_cache(None)
-def f(x, y, c):  # ставим счётчик на c
+def function2(x, y, c):  # ставим счётчик на c
     if x == y:
         a.add(c)  # добавляем в set
         return x == y
     if x > y: return 0
-    return f(x + 1, y, c + 1) + f(x + 5, y, c + 1) + f(x * 3, y, c + 1)  # по условию задачи
+    return function2(x + 1, y, c + 1) + function2(x + 5, y, c + 1) + function2(x * 3, y, c + 1)  # по условию задачи
 
 
 a = set()
-f(1, 227, 0)
+function2(1, 227, 0)
 print(min(a))  # находим минимальную длину
 
 
 # другие задачи
 # 1
-def f(x, y):
+def function3(x, y):
     if x >= y: return x == y
-    return f(x + 2, y) + f(x + 4, y) + f(x + 5, y)
+    return function3(x + 2, y) + function3(x + 4, y)  + function3(x + 5, y)
 
 
 for i in range(1, 1000):
-    if f(31, i) == 1001:
+    if function3(31, i) == 1001:
         print(i)
 # 2
 # на кол во различных чисел которые могут получиться
@@ -46,14 +51,14 @@ from functools import *
 
 
 @lru_cache(None)
-def f(x, k):
+def function4(x, k):
     if k == 68:
         a.add(x)
         return
-    f(x + 1, k + 1)
-    f(x + 3, k + 1)
+    function4(x + 1, k + 1)
+    function4(x + 3, k + 1)
 
 
 a = set()
-f(1, 0)
+function4(1, 0)
 print(len(a))
