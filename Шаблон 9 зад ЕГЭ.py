4345b783ef318422e9d7beaad51c5849224bcdc3
n1k <nik.borgoyakov@bk.ru>
2024-12-03 20:07:12 +0700

    COMMIT fix functions in 14 task

--- Шаблон 9 зад ЕГЭ.py
+++ Шаблон 9 зад ЕГЭ.py
@@ -1,9 +1,8 @@
-with open('9.txt') as file: # лучше решать 9 задание прогой
+with open('9.txt') as file:
+    # лучше решать 9 задание прогой если сложное решение
     final = 0
     for i in file:
         a, b, c, d = sorted([int(k) for k in i.split()])
-    """классический перевод чисел """
-        if d ** 3 > 2 * a * b * c and a > 10 and c > 10 \
-                and b > 10 and d > 10:
+        if d ** 3 > 2 * a * b * c and a > 10 and c > 10 and b > 10 and d > 10:
             final += 1
     print(final)
