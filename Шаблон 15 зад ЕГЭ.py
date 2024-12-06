efb27488bcc9f029b00506915da4328357490da8
n1k17 <nik.borgoyakov@bk.ru>
2024-12-07 06:32:30 +0700

    COMMIT add comments in 15 task

--- Шаблон 15 зад ЕГЭ.py
+++ Шаблон 15 зад ЕГЭ.py
@@ -1,16 +1,14 @@
-b = [k for k in range(24, 91)] # список для В
-c = [k for k in range(47, 116)] # список для С
+b = list(range(24, 91)) # список для В
+c = list(range(47, 116)) # список для С
 m = 10000 # максимальное число
-for a_1 in range(0, 100): # старт
-    for a_2 in range(a_1 + 1, 100): # конец
+for left in range(0, 100): # старт
+    for right in range(left + 1, 100): # конец
         count = 0 # счётчик
-        A = [k for k in range(a_1, a_2 + 1)]
+        A = list(range(left, right + 1))
         for x in range(-100, 100):
-            f = ((x in c) <= (((not(x in A)) and (x in b)) <= (not(x in c))))
-            if f == False: # исключаем A
+            if ((x in c) <= (((not(x in A)) and (x in b)) <= (not(x in c)))) == False:
                 count = 1
                 break
         if count == 0:
-            m = min(m, a_2 - a_1 + 1) # нахождение минимальной длины
-print(m - 1) # нужно сделать -1
-print(m) # но в других задачах -1 не надо делать
+            m = min(m, right - left + 1) # нахождение минимальной длины
+
