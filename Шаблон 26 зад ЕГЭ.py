2e5debf705a4fae453744deaec367b9185ce037e
n1k17 <nik.borgoyakov@bk.ru>
2024-12-06 07:09:06 +0700

    COMMIT modification comments and code in 26 task

--- Шаблон 26 зад ЕГЭ.py
+++ Шаблон 26 зад ЕГЭ.py
@@ -1,6 +1,3 @@
-print('Я поступлю в ИТМО')
-
-
 """
 В магазине для упаковки подарков есть N кубических коробок. 
 Самой интересной считается упаковка подарка по принципу 
@@ -14,7 +11,7 @@ print('Я поступлю в ИТМО')
 маленькой коробки, где будет находиться подарок. 
 Размер подарка позволяет поместить его в самую 
 маленькую коробку."""
-with open('26 (2).txt') as file:
+with open('...') as file:
     n = int(file.readline())
     data = [int(k) for k in file]
 data.sort(reverse=True)
@@ -26,54 +23,54 @@ for i in range(n):
             max_count.append(data[i])
     print(min(max_count), len(max_count))
 
+"""
+На грузовом судне необходимо перевезти 
+контейнеры, имеющие одинаковый габарит и 
+разные массы (некоторые контейнеры могут 
+иметь одинаковую массу). Общая масса всех 
+контейнеров превышает грузоподъёмность судна. 
+Количество грузовых мест на судне не меньше 
+количества контейнеров, назначенных к перевозке. 
+Какое максимальное количество контейнеров можно 
+перевезти за один рейс и какова масса самого 
+тяжёлого контейнера среди всех контейнеров, 
+которые можно перевезти за один рейс
+"""
 
-"""На грузовом судне необходимо перевезти \
-контейнеры, имеющие одинаковый габарит и \
-разные массы (некоторые контейнеры могут \
-иметь одинаковую массу). Общая масса всех \
-контейнеров превышает грузоподъёмность судна. \
-Количество грузовых мест на судне не меньше \
-количества контейнеров, назначенных к перевозке. \
-Какое максимальное количество контейнеров можно \
-перевезти за один рейс и какова масса самого \
-тяжёлого контейнера среди всех контейнеров, \
-которые можно перевезти за один рейс"""
 with open('26 (1).txt') as file:
     s, n = map(int, file.readline().split())
     data = sorted([int(k) for k in file])
 sum_s = 0
 for count in range(n):
     if (sum_s + data[count]) > s:
-        # просто перебираем с миинимальной суммы
+        # просто перебираем с минимальной суммы
         break
     sum_s += data[count]
-print(count) # выводим всю длину
-zapas = s - sum_s # запас
-for i in range(n): # снова перебор
+print(count)  # выводим всю длину
+zapas = s - sum_s  # запас
+for i in range(n):  # снова перебор
     if data[i] - data[count - 1] <= zapas:
         # если разница между числом и номером с минус один \
         # меньше, либо равно запасу то он
-        the_end = data[i] # и является концом
+        the_end = data[i]  # и является концом
 print(the_end)
 
-
-
-
-
-"""В сервисе «Капчеломы» N работников разгадывают
+"""
+В сервисе «Капчеломы» N работников разгадывают
 капчи. Разгадывание каждой капчи каждым работником
 занимает ровно 60 секунд, разгадывать две капчи
-одновременно нельзя.Имеется K запросов на прохождение \
-капчи, про каждый известно время в секундах от начала \
-рабочего дня, когда этот запрос появляется. \
-Если не взять запрос в работу сразу же, \
-его перехватывает сервис конкурентов, \
-и выполнить его уже нельзя."""
+одновременно нельзя.Имеется K запросов на прохождение 
+капчи, про каждый известно время в секундах от начала 
+рабочего дня, когда этот запрос появляется. 
+Если не взять запрос в работу сразу же, 
+его перехватывает сервис конкурентов, 
+и выполнить его уже нельзя.
+"""
 
-with open('26.txt') as file:
+with open('...') as file:
     n, r = map(int, file.readline().split())
     s = [int(k) for k in file]
-s.sort()  # сортируем файлы
+s.sort()
 sum_s = 0
 min_time = 10 ** 11
 for i in range(n):
@@ -91,7 +88,7 @@ for i in range(n):
     s = [k for k in s if k != -1]  # обновление файлов
 print(s, n)
 
-with open('26.1.txt') as file:
+with open('...') as file:
     k = 0
     s = []
     for i in file:
@@ -111,7 +108,7 @@ with open('26.1.txt') as file:
     print(final)
     print(l, max(final))
 
-with open('26_14959 (1).txt') as file:
+with open('...') as file:
     point, time = map(int, file.readline().split())  # находим данные для решения
     s = [(int(k.split()[0]), int(k.split()[0]) + int(k.split()[1])) for k in file]
 s.sort(key=lambda t: (t[1], t[0]))  # метод сортировки кортежей
@@ -130,7 +127,7 @@ for x, y in s:
             last_min = x
 print(last_min)
 
-with open('26 (5).txt') as file:
+with open('...') as file:
     count = map(int, file.readline().split())
     time_day = [0 for k in range(0, 1441)]  # список с нулями
     for i in file:
@@ -150,7 +147,7 @@ with open('26 (5).txt') as file:
     the_end = [1 for k in time_day if k == 0]
     print(sum(the_end) - 1)
 
-with (open('26_10107.txt') as file):
+with open('...') as file:
     counter = int(file.readline())  # counter numbers in list
     time = [list(map(int, i.split())) for i in file]  # all time
 time.sort(key=lambda i: i[-1])  # sorting by last
@@ -164,8 +161,10 @@ for start, finish in taken[1:]:
         longest = max(longest, start - taken[-2][1])  # between holidays
 print(len(taken), longest)  # answer
 
-"""или можно так"""
-with open('26_2024.txt') as file:
+"""
+или можно так
+"""
+with open('...') as file:
     n = int(file.readline())
     data = [list(map(int, i.split())) for i in file]
 data.sort(key=lambda x: x[-1])
@@ -180,8 +179,7 @@ for i in data:
         max_len = max(max_len, i[0])
 print(len(time), max_len - time[-2])
 
-
-with open('26_15341 (1).txt') as file:
+with open('...') as file:
     counter = int(file.readline())  # кол во отрезков
     favorite_music, other_music = [], []
     for i in range(counter):
@@ -189,14 +187,22 @@ with open('26_15341 (1).txt') as file:
         if number == 1:
             favorite_music.append((start, start + final))  # начало и длина до конца
         else:
-            other_music.append((start, start + final))  # тоже самое
+            other_music.append((start, start + final))  # то же самое
 favorite_music = sorted(favorite_music)  # сортируем данные
 other_music = sorted(other_music, key=lambda x: x[1])  # сортировка по последнему числу
+
+"""------------------------------------------------------------------------------------------"""
+
+
 def check(start, final):
     global favorute_music  # открывает переменные вне функции
     for start_1, final_1 in favorite_music:
-        if start_1 > final: return True
-        if start < final_1 and final > start_1:  return False
+        if start_1 > final:
+            return True
+        if start < final_1 and final > start_1:
+            return False
+
+
 used_other_music = []
 for start, final in other_music:
     if len(used_other_music) > 0:
@@ -208,9 +214,7 @@ print(len(used_other_music) + len(favorite_music))  #
 last_music = [i for i in other_music if check(i[0], i[1]) and i[0] >= used_other_music[-2][1]]
 print(last_music[-1][1])  # ответ
 
-
-
-
+"""--------------------------------------------------------------------------------------------------"""
 
 with open('27-B_108.txt') as file:
     n = file.readline()
@@ -227,10 +231,12 @@ if sum_s % 5 == 0:  # если неделится
     sum_s -= min_s  # то отнимаем минимум
 print(sum_s)
 
-"""На вход программы поступает последовательность из N пар \
-целых положительных чисел. Необходимо выбрать из каждой пары \
-число таким образом, чтобы сумма выбранных чисел не делилась \
-на 5, при этом была максимальной возможной. """
+"""
+На вход программы поступает последовательность из N пар 
+целых положительных чисел. Необходимо выбрать из каждой пары 
+число таким образом, чтобы сумма выбранных чисел не делилась 
+на 5, при этом была максимальной возможной. 
+"""
 
 
 def max_del(n):
@@ -244,8 +250,6 @@ def max_del(n):
 
 
 for i in range(224466, 664422 + 1):
-    if i % 5 == 0 and i % 7 == 0 and \
-            i % 13 == 0 and i % (5 ** 2) != 0 \
-            and i % (7 ** 2) != 0 and i % (13 ** 2) != 0 \
-            and max_del(i) <= 10 ** 5:
+    if i % 5 == 0 and i % 7 == 0 and i % 13 == 0 and i % (5 ** 2) != 0 and i % (7 ** 2) != 0 and i % (
+            13 ** 2) != 0 and max_del(i) <= 10 ** 5:
         print(i, max_del(i))
