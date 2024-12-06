89b8cbd5cc4f204cc171fc32ab1ab965bbd66449
n1k17 <nik.borgoyakov@bk.ru>
2024-12-07 06:36:24 +0700

    COMMIT add comments and code in 16 task

--- Шаблон 16 зад ЕГЭ.py
+++ Шаблон 16 зад ЕГЭ.py
@@ -1,10 +1,25 @@
-from functools import lru_cache
 import sys
-sys.setrecursionlimit(4000) # увеличиваем рекурсию
-@lru_cache(None) # сокращает код
-def F(n):  # по условию задачи
-    if n == 1: return 1
-    if n > 1: return n * F(n - 1) - 1
+from functools import lru_cache
+
+sys.setrecursionlimit(4000)  # увеличиваем рекурсию
+
+"-----------------------------------------------------------------------------"
+"""
+Тут мы без lru cache переписываем рекурсию
+а дальше если мы превышаем лимит, то увеличиваем или оптимизируем 
+setrecursionlimit или @lru_cache
+"""
+
+
+@lru_cache(None)  # сокращает код
+def F(number):  # по условию задачи
+    if number == 1:
+        return 1
+    if number > 1:
+        return number * F(number - 1) - 1
+
+
+"-----------------------------------------------------------------------------"
 for i in range(1, 1001):
-    F(i) # если ничто не работаем, то перебираем в цикле
+    F(i)  # если ничто не работаем, то перебираем в цикле
 print(F(1000) / F(997))
