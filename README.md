3abb509d8eee6038d42c235adce2212f436020ef
n1k17 <nik.borgoyakov@bk.ru>
2024-12-06 07:48:20 +0700

    COMMIT READMI.md

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Hello !!!
 This is my files in Python code for school learning.
 In 2024 year i learn in test ЕГЭ.
 
-# Please 🙏 don't hate me. My code is not very good, but I was a very stupid and I don't understand PEP 8
+
+
+
+
+
+# Please 🙏 don't hate me. My code is not very good, but I was a very stupid and I don't know about PEP 8
--- stepic traveling.py
+++
@@ -1,9 +0,0 @@
-count = int(input())
-sum_True = 0
-data = []
-for _ in range(count):
-    answer = str(input())
-    sum_True += 1 if answer.split()[-1] == 'Correct' and answer not in data else 0
-    data.append(answer)
-print(f'Верно решили {sum_True} учащихся')
-print(f'Из всех попыток {int(sum_True / len(set(data)) * 100)}% верных')
